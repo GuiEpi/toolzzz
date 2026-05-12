6f6edba3b504261dab7f7d6150f2f583262caa10
GuiEpi <guillaume.coussot@epitech.eu>
2026-05-12 22:32:08 +0200

    build(release): bump version to 3.5.2

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   manifest: {
     name: "Toolzzz",
     description: "Extension pour www.fourmizzz.fr.",
-    version: "3.5.1",
+    version: "3.5.2",
     // @ts-expect-error AMO refuse la forme objet; Chrome accepte la string. On s'aligne sur Firefox.
     author: "Hraesvelg",
     homepage_url: "https://github.com/GuiEpi/toolzzz",
